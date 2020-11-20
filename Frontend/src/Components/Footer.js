d9a9ed5c8b4292addf26cd66ef58fe9ccf1433aa
Arturoo0 <arturoportelles@gmail.com>
2020-11-20 13:31:20 -0500

    added links and modified footer

--- Frontend/src/Components/Footer.js
+++ Frontend/src/Components/Footer.js
@@ -7,12 +7,12 @@ class Footer extends React.Component {
         return(
             <div id='footer-root'>
                 <div id='footer-name-date'>
-                    Arturo Portelles
+                    ARTURO PORTELLES
                 </div>
                 <div id='footer-links-container'>
                     <ul id='footer-social-links'>
-                        <li>-</li>
-                        <li>-</li>
+                        <li><i class="fab fa-github-square"></i></li>
+                        <li><i class="fab fa-linkedin"></i></li>
                     </ul>
                 </div>
             </div>
